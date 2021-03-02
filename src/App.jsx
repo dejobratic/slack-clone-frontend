48ce825b9adcb99eae6375a3263d9b8f52db90e4
dejobratic <dejobratic@gmail.com>
2021-03-02 20:21:59 +0100

    Added chat hub using signalR.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react"
+import Chat from "Chat"
 
 const App = () => {
-  return <div>Hello, world!</div>
+  return (
+    <div style={{ margin: "0 30%" }}>
+      <Chat />
+    </div>
+  )
 }
 
 export default App
